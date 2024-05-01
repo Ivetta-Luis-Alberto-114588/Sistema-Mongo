aac96eb30edffd6874d3c77648009593a65c44e5
114588-Luis Ivetta <114588@tecnicatura.frc.utn.edu.ar>
2024-05-01 10:17:58 -0300

    modificaciono auth controller, index  y google verify

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -54,13 +54,53 @@ const login = async (req, resp = response) =>{
     }
 }
 
-
-//para ver el token entrar a http://localhost:3000/
+/*
+    esta funcion va a recibir el token de google en el req y con ese token se extrae el mail del payload
+    y con ese mail se busca en nuestra BD, si se encuentra el usuario en la BD nuestra se le pone la foto 
+    de google
+    Si no se encuentra se crea un usuario sin contraseña en nuestra BD
+*/
 const googleSignIn = async (req, resp = response) => {
+    //para ver el token entrar a http://localhost:3000/
     
+/**
+ * ESTO ES UN EJEMPLO DE LO QUE SE OBTIENE DEL PAYLOAD DE GOOGLE
+ * https://developers.google.com/identity/gsi/web/reference/js-reference?hl=es-419
+ */
+
+    //     header
+// {
+//   "alg": "RS256",
+//   "kid": "f05415b13acb9590f70df862765c655f5a7a019e", // JWT signature
+//   "typ": "JWT"
+// }
+//          payload
+// {
+//   "iss": "https://accounts.google.com", // The JWT's issuer
+//   "nbf":  161803398874,
+//   "aud": "314159265-pi.apps.googleusercontent.com", // Your server's client ID
+//   "sub": "3141592653589793238", // The unique ID of the user's Google Account
+//   "hd": "gmail.com", // If present, the host domain of the user's GSuite email address
+//   "email": "elisa.g.beckett@gmail.com", // The user's email address
+//   "email_verified": true, // true, if Google has verified the email address
+//   "azp": "314159265-pi.apps.googleusercontent.com",
+//   "name": "Elisa Beckett",
+//                             // If present, a URL to user's profile picture
+//   "picture": "https://lh3.googleusercontent.com/a-/e2718281828459045235360uler",
+//   "given_name": "Elisa",
+//   "family_name": "Beckett",
+//   "iat": 1596474000, // Unix timestamp of the assertion's creation time
+//   "exp": 1596477600, // Unix timestamp of the assertion's expiration time
+//   "jti": "abc161803398874def"
+// }
+
+
+
+
     try {
         // const googleUser = await googleVerify ( req.body.token )
-        //desectructuro la info del usuario
+        //desectructuro la info del usuario, lo que voy a usar es el 
+        // mail para buscar los datos en mi BD y cuando ese dato coincide devuelvo un objeto
         const {email, name, picture} = await googleVerify ( req.body.token )
         console.log ("respuesta de google cuando se ejectua el metodo googleSignIn de authController:"
                         , email
@@ -72,6 +112,9 @@ const googleSignIn = async (req, resp = response) => {
         const usuarioDB = await Usuario.findOne( { email })
         let usuario
 
+        //  si no existe ningun usuario en mi BD con ese mail se crea uno
+        // si existe un usuario en la BD le pongo las propiedades y despues le sobreescribo algunas propiedades
+        // por ejemplo la img, 
         if (!usuarioDB) {
             usuario = new Usuario({
                 nombre: name, //asingo a la propiedad nombre el name de google
@@ -90,12 +133,13 @@ const googleSignIn = async (req, resp = response) => {
             //usuario.password = '@@'
         }
 
-        //guardo el usuario
+        //guardo el usuario en la BD
         await usuario.save();
         
         //generar el token
         const token = await generarJwt( usuario._id ) 
 
+        // esto devuelve mi ENDPOINT email name y picture TODO DE GOOGLE y el token que acabo de generar con el payload de _id
         resp.json({
             ok: true,
             email, name, picture,
--- helpers/google-verify.helpers.js
+++ helpers/google-verify.helpers.js
@@ -1,11 +1,17 @@
+//importo la libreria
 const {OAuth2Client} = require('google-auth-library');
+
+//creo un instancia de la liberia con el secreto mio de google
 const client = new OAuth2Client( process.env.GOOGLE_SECRET );
 
+
+
+// creo una funcion asincrona, a la cual hay que pasarla el TOKEN de GOOGLE
+// , que va  devolver el NAME, EMAIL, IMAGEN de GOOGLE
 async function googleVerify( token ) {
-    
-  const ticket = await client.verifyIdToken({
-      idToken: token,
-      audience: process.env.GOOGLE_ID,  // Specify the CLIENT_ID of the app that accesses the backend
+  
+  //el token es el del USUARIO que se logea, y audience es el MIO que est en el .ENV
+  const ticket = await client.verifyIdToken({ idToken: token, audience: process.env.GOOGLE_ID,  // Specify the CLIENT_ID of the app that accesses the backend
       // Or, if multiple clients access the backend:
       //[CLIENT_ID_1, CLIENT_ID_2, CLIENT_ID_3]
   });
